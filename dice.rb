9152bd0d40eb9b91aa04aec669d8b34fc911ccb7
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-07-28 17:42:59 +0000

    Accept changes without restarting the server. Add comments and hello world

--- dice.rb
+++ dice.rb
@@ -1,7 +1,19 @@
 # /dice.rb
 
 require "sinatra"
+require "sinatra/reloader"
 
+# / , The root
+get("/") do
+  "Hello World"
+end
+
+# /zebra
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+# /giraffe
+get(/giraffe" do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
